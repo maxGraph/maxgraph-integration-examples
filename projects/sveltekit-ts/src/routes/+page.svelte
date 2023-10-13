42994e46f8fae18e4aebcdc21ae21f7715901a13
Mirco Veltri <indaco@users.noreply.github.com>
2023-10-13 11:01:21 +0200

    feat: add sveltekit ts project example (#46)
    This adds a sample project to showcase how to use maxGraph with SvelteKit.
    
    ---------
    
    Co-authored-by: Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>

---
+++ projects/sveltekit-ts/src/routes/+page.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+  import {
+    Graph,
+    InternalEvent,
+    Perimeter,
+    RubberBandHandler,
+  } from '@maxgraph/core';
+  import { onMount } from 'svelte';
+  import { registerCustomShapes } from '$lib/custom-shapes';
+
+  let container: HTMLElement;
+
+  onMount(async () => {
+    // Disables the built-in context menu
+    InternalEvent.disableContextMenu(container);
+
+    const graph = new Graph(container);
+    graph.setPanning(true); // Use mouse right button for panning
+    new RubberBandHandler(graph); // Enables rubber band selection
+
+    // shapes and styles
+    registerCustomShapes();
+    // create a dedicated style for "ellipse" to share properties
+    graph.getStylesheet().putCellStyle('myEllipse', {
+      perimeter: Perimeter.EllipsePerimeter,
+      shape: 'ellipse',
+      verticalAlign: 'top',
+      verticalLabelPosition: 'bottom',
+    });
+
+    // Gets the default parent for inserting new cells. This
+    // is normally the first child of the root (ie. layer 0)
+    const parent = graph.getDefaultParent();
+
+    // Adds cells to the model in a single step
+    graph.batchUpdate(() => {
+      // use the legacy insertVertex method
+      const vertex01 = graph.insertVertex(
+        parent,
+        null,
+        'rectangle',
+        10,
+        10,
+        100,
+        100
+      );
+      const vertex02 = graph.insertVertex(
+        parent,
+        null,
+        'ellipse',
+        350,
+        90,
+        50,
+        50,
+        {
+          baseStyleNames: ['myEllipse'],
+          fillColor: 'orange',
+        }
+      );
+      // use the legacy insertEdge method
+      graph.insertEdge(parent, null, 'edge', vertex01, vertex02, {
+        // TODO cannot use constants.EDGESTYLE.ORTHOGONAL
+        // TS2748: Cannot access ambient const enums when the '--isolatedModules' flag is provided.
+        // See https://github.com/maxGraph/maxGraph/issues/205
+        edgeStyle: 'orthogonalEdgeStyle',
+        rounded: true,
+      });
+
+      // insert vertex using custom shapes using the new insertVertex method
+      const vertex11 = graph.insertVertex({
+        parent,
+        value: 'a custom rectangle',
+        position: [20, 200],
+        size: [100, 100],
+        style: { shape: 'customRectangle' },
+      });
+      // use the new insertVertex method using position and size parameters
+      const vertex12 = graph.insertVertex({
+        parent,
+        value: 'a custom ellipse',
+        x: 150,
+        y: 350,
+        width: 70,
+        height: 70,
+        style: {
+          baseStyleNames: ['myEllipse'],
+          shape: 'customEllipse',
+        },
+      });
+      // use the new insertEdge method
+      graph.insertEdge({
+        parent,
+        value: 'another edge',
+        source: vertex11,
+        target: vertex12,
+        style: { endArrow: 'block' },
+      });
+    });
+  });
+</script>
+
+<div bind:this={container} id="graph-container" />
