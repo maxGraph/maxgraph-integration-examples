5f0000cd4e53264855c141ddafca982ac9e79170
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-10-13 15:09:37 +0200

    WIP code

---
+++ projects/_shared/src/custom-shapes.ts
@@ -0,0 +1,58 @@
+import type {AbstractCanvas2D, ColorValue, Rectangle} from '@maxgraph/core';
+import {CellRenderer, EllipseShape, RectangleShape} from '@maxgraph/core';
+
+export function registerCustomShapes(): void {
+  console.info('Registering custom shapes...');
+  // @ts-ignore TODO fix CellRenderer. Calls to this function are also marked as 'ts-ignore' in CellRenderer
+  CellRenderer.registerShape('customRectangle', CustomRectangleShape);
+  // @ts-ignore
+  CellRenderer.registerShape('customEllipse', CustomEllipseShape);
+  console.info('Custom shapes registered');
+}
+
+class CustomRectangleShape extends RectangleShape {
+  constructor(bounds: Rectangle, fill: ColorValue, stroke: ColorValue) {
+    super(bounds, fill, stroke, 3);
+    this.isRounded = true; // force rounded shape
+  }
+
+  paintBackground(
+    c: AbstractCanvas2D,
+    x: number,
+    y: number,
+    w: number,
+    h: number
+  ): void {
+    c.setFillColor('Chartreuse');
+    super.paintBackground(c, x, y, w, h);
+  }
+
+  paintVertexShape(
+    c: AbstractCanvas2D,
+    x: number,
+    y: number,
+    w: number,
+    h: number
+  ) {
+    c.setStrokeColor('Black');
+    super.paintVertexShape(c, x, y, w, h);
+  }
+}
+
+class CustomEllipseShape extends EllipseShape {
+  constructor(bounds: Rectangle, fill: string, stroke: string) {
+    super(bounds, fill, stroke, 5);
+  }
+
+  paintVertexShape(
+    c: AbstractCanvas2D,
+    x: number,
+    y: number,
+    w: number,
+    h: number
+  ) {
+    c.setFillColor('Yellow');
+    c.setStrokeColor('Red');
+    super.paintVertexShape(c, x, y, w, h);
+  }
+}
