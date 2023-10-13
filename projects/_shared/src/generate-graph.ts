b648364c84f307d212639ab02e96138e1bcd1fa7
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-10-13 16:07:42 +0200

    shared: remove out dated comment

--- projects/_shared/src/generate-graph.ts
+++ projects/_shared/src/generate-graph.ts
@@ -12,7 +12,6 @@ export const initializeGraph = (container: HTMLElement) => {
 
     const graph = new Graph(container);
     graph.setPanning(true); // Use mouse right button for panning
-    // WARN: as the maxGraph css files are not available in the npm package (at least for now), dedicated CSS class must be defined in style.css
     new RubberBandHandler(graph); // Enables rubber band selection
 
     // shapes and styles
