a6c17596932cf75007ea43f4e141b094988ee82d
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-08-19 00:57:57 +0200

    give a try to chunk "split-by-module"

--- projects/rsbuild-ts/rsbuild.config.ts
+++ projects/rsbuild-ts/rsbuild.config.ts
@@ -1,3 +1,15 @@
 import { defineConfig } from '@rsbuild/core';
 
-export default defineConfig({});
+export default defineConfig({
+    // enforce a chunk per module to better track the size of the maxGraph chunk.
+    // We don't need it here because we have a single dependency, so the chunk automatically generated only includes the maxGraph module.
+    // In addition, this also split chunks for the CSS files, which is not what we want (at least for now).
+    // https://rsbuild.dev/guide/optimization/split-chunk#split-by-module
+    // performance: {
+    //     chunkSplit: {
+    //         strategy: 'split-by-module',
+    //     },
+    // },
+});
+
+
