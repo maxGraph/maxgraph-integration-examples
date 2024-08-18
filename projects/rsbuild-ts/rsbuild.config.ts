0969f92401a6248803e7a2781a31f38d327b54b0
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-08-19 01:05:26 +0200

    configure favicon

--- projects/rsbuild-ts/rsbuild.config.ts
+++ projects/rsbuild-ts/rsbuild.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from '@rsbuild/core';
 
 export default defineConfig({
+    html: {
+        favicon: './assets/favicon-128x128.png',
+    },
     // enforce a chunk per module to better track the size of the maxGraph chunk.
     // We don't need it here because we have a single dependency, so the chunk automatically generated only includes the maxGraph module.
     // In addition, this also split chunks for the CSS files, which is not what we want (at least for now).
