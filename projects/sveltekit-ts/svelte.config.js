b4c4409040d45e01fb0583fe3c8f3bf94544964e
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-08-19 17:00:46 +0200

    feat: deploy the sveltekit project on GitHub Pages
    Switch from `@sveltejs/adapter-auto` to `@sveltejs/adapter-static` (use v2 because v3 is for sveltekit v2) to be able to create a SPA.
    Downgrade `vite` to v4 because `@sveltejs/adapter-static` doesn't support vite v5
    
    In addition, bump svelte dependencies
      - svelte from 4.2.8 to 4.2.18
      - svelte-check to 3.8.5
      - @sveltejs/kit from 1.28.0 to 1.30.4 (migration to v2 will be done later)

--- projects/sveltekit-ts/svelte.config.js
+++ projects/sveltekit-ts/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		// For GitHub Pages, use https://kit.svelte.dev/docs/adapter-static
+		adapter: adapter({
+			// In https://kit.svelte.dev/docs/adapter-static#github-pages, the page '404.html' is used as a fallback.
+			// We cannot use it here because the app is deployed to a subdirectory. The 404 page for the deployment should be placed in the root of the deployment.
+			// The configured callback lets access to https://maxgraph.github.io/maxgraph-integration-examples/maxgraph-integration-examples/sveltekit-ts
+			fallback: 'index.html'
+		}),
+		paths: {
+			// We are deploying to a subdirectory as we have several applications in the same repository
+			base: process.argv.includes('dev') ? '' : '/maxgraph-integration-examples/sveltekit-ts'
+		}
 	}
 };
 
