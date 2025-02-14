3be7c1b9a55e7ca8471786322d721d13be1ae50a
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2025-02-14 18:26:56 +0100

    feat: add an example using the farm build tool (#191)

---
+++ projects/farm-ts/src/main.ts
@@ -0,0 +1,23 @@
+import '@maxgraph/core/css/common.css';
+import 'maxgraph-examples-shared/css/rubber-band.css'
+import 'maxgraph-examples-shared/css/general-style.css'
+
+import {Client} from "@maxgraph/core";
+import {initializeGraph} from 'maxgraph-examples-shared';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+    <h1>maxGraph Farm TypeScript example</h1>
+    <p>Display a test graph. Activated behaviours:</p>
+    <ul>
+      <li>Panning: use mouse right button</li>
+      <li>Cells selection with Rubberband: use mouse left button</li>
+    </ul>
+    <div id="graph-container"></div>
+    <footer></footer>
+`;
+
+const footer = document.querySelector<HTMLElement>('footer')!;
+footer.innerText = `Built with maxGraph ${Client.VERSION}`;
+
+// Creates the graph inside the given container
+initializeGraph(<HTMLElement>document.querySelector('#graph-container'));
